e08a9a23ecd4e37a4b984f02a439b83fe008d584
SergeyParadigm77 <paradigm77@rambler.ru>
2023-02-02 23:08:02 +0300

    ДЗ выполнено!

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Book alchemist = new Book("Алхимик", 1988);
+        alchemist.setYearPublication(1995);
+        Author coelho = new Author("Паоло", "Коэльо");
+        Book scaffold = new Book("Плаха", 1986);
+        Author aitmatov = new Author("Чингиз", "Айтматов");
+
+        System.out.println("Наименование книги - " + alchemist.getName() + ", " + "Год издания - " + alchemist.getYearPublication() + ", " + "Имя автора - " + coelho.getFirstName() + ", " + "Фамилия автора - " + coelho.getLastName());
+        System.out.println("Наименование книги - " + scaffold.getName() + ", " + "Год издания - " + scaffold.getYearPublication() + ", " + "Имя автора - " + aitmatov.getFirstName() + ", " + "Фамилия автора - " + aitmatov.getLastName());
     }
 }
