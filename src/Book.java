4155c7f67b8d3795a612a98532cf309544f41363
SergeyParadigm77 <paradigm77@rambler.ru>
2023-02-04 10:12:20 +0300

    В класс Book добавлено поле author!

--- src/Book.java
+++ src/Book.java
@@ -1,11 +1,13 @@
 public class Book {
     private String name;
     private int yearPublication;
+    private Author author;
 
 
-    public Book(String name, int yearPublication) {
+    public Book(String name, int yearPublication, Author author) {
         this.name = name;
         this.yearPublication = yearPublication;
+        this.author = author;
     }
 
     public String getName() {
@@ -14,6 +16,11 @@ public class Book {
     public int getYearPublication() {
         return yearPublication;
     }
+
+    public Author getAuthor() {
+        return author;
+    }
+
     public void setYearPublication(int yearPublication) {
         this.yearPublication = yearPublication;
     }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        Book alchemist = new Book("Алхимик", 1988);
-        alchemist.setYearPublication(1995);
         Author coelho = new Author("Паоло", "Коэльо");
-        Book scaffold = new Book("Плаха", 1986);
+        Book alchemist = new Book("Алхимик", 1988, coelho);
+        alchemist.setYearPublication(1995);
         Author aitmatov = new Author("Чингиз", "Айтматов");
+        Book scaffold = new Book("Плаха", 1986, aitmatov);
 
-        System.out.println("Наименование книги - " + alchemist.getName() + ", " + "Год издания - " + alchemist.getYearPublication() + ", " + "Имя автора - " + coelho.getFirstName() + ", " + "Фамилия автора - " + coelho.getLastName());
-        System.out.println("Наименование книги - " + scaffold.getName() + ", " + "Год издания - " + scaffold.getYearPublication() + ", " + "Имя автора - " + aitmatov.getFirstName() + ", " + "Фамилия автора - " + aitmatov.getLastName());
+        System.out.println("Наименование книги - " + alchemist.getName() + ", Год издания - " + alchemist.getYearPublication() + ", Имя автора - " + alchemist.getAuthor().getFirstName() + ", Фамилия автора - " + alchemist.getAuthor().getLastName() );
+        System.out.println("Наименование книги - " + scaffold.getName() + ", Год издания - " + scaffold.getYearPublication() + ", Имя автора - " + scaffold.getAuthor().getFirstName() + ", Фамилия автора - " + scaffold.getAuthor().getLastName());
     }
 }
